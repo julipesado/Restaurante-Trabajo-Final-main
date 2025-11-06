48d812ff4259bb345fb26dd7edcd6e1a19702b83
micaelaasteE <micaelaaste1@gmail.com>
2025-11-06 11:41:00 -0300

    cambios 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,24 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { onlyPublicUserGuard } from './guards/only-public-user-guard-guard';
 import { ExplorePage } from './explore-page/explore-page';
-import { RegisterPage } from './register-page/register-page';
-import { LoggedLayout } from './logged-layout/logged-layout';
-import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
+import { RestaurantPage } from './restaurant-page/restaurant-page';
 
 export const routes: Routes = [
     {
         path: "login",
         component : LoginPage,
+        canActivate: [onlyPublicUserGuard]
     },
     {
         path: "",
         component: ExplorePage,
-    },
-    {
-        path: "register",
-        component: RegisterPage
-    },
-    {
-        path: "logged-layout",
-        component: LoggedLayout,
-        canActivate: [onlyLoggedUserGuard]
+        canActivateChild: [onlyPublicUserGuard],
+        children: [
+            {
+                path: "",
+                component: RestaurantPage
+            }
+        ]
     }
 ];
